7708d2bd5dbc129874f63ee7da676814fcd14309
Akshat Chowdhry <akshatchowdhry15@gmail.com>
2024-08-08 22:09:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const greetingElement = document.getElementById('greeting');
+    greetingElement.textContent = 'Hello, Welcome to My Vanilla JS Project!';
+});
